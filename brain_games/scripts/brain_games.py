b19528ff82bf637f54c1501a2fd8547997a35d27
xantol <xantol@rambler.ru>
2024-05-31 16:10:50 +0500

    update brain_games.py

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 
-def main():
-    print('Welcome to the Brain Games!')
+from brain_games.cli import welcome_user
 
+def main():
+    welcome_user()
 
 if __name__ == '__main__':
     main()
+
