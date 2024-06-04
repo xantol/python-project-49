207acd4f8778faacbf32972ebea752fd1e9b7b54
xantol <xantol@rambler.ru>
2024-06-04 18:10:29 +0500

    remake brain-calc module and script

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -1,29 +1,17 @@
-import prompt
-from brain_games.games.greet import welcome_user
 from random import randint, choice
 
 
-def brain_calc():
-    user = welcome_user()
-    print('What is the result of the expression?')
-    for _ in range(3):
-        first_num = randint(1, 20)
-        second_num = randint(1, 50)
-        function = choice(['*', '+', '-'])
-        print(f'Question: {first_num} {function} {second_num}')
-        answer = prompt.string('Your answer: ')
-        if function == '+':
-            correct_answer = first_num + second_num
-        elif function == '-':
-            correct_answer = first_num - second_num
-        else:
-            correct_answer = first_num * second_num
-        if answer == str(correct_answer):
-            print('Correct!')
-        else:
-            print(
-                f"'{answer}' is wrong answer ;(. Correct answer was "
-                f"'{correct_answer}'.\nLet's try again, {user}!"
-            )
-            return
-    print(f'Congratulations, {user}!')
+question = 'What is the result of the expression'
+
+def play_game():
+    num1 = randint(1, 100)
+    num2 = randint(1, 100)
+    operator = choice(['+', '-', '*'])
+    print(f'{num1} {operator} {num2}')
+    if operator == '+':
+        correct_answer = num1 + num2
+    elif operator == '-':
+        correct_answer = num1 - num2
+    else:
+        correct_answer = num1 * num2
+    return str(correct_answer)
--- brain_games/scripts/brain_calc.py
+++ brain_games/scripts/brain_calc.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 
-from brain_games.games.calc import brain_calc
+from brain_games.engine import play_engine
 
 
 def main():
-    brain_calc()
+    play_engine(calc)
 
 
 if __name__ == '__main__':
