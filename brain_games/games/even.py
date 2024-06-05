39f5cb74aa2afe27afc3e2298aaeada47c051f28
xantol <xantol@rambler.ru>
2024-06-06 00:06:00 +0500

    make lint in even.py

--- brain_games/games/even.py
+++ brain_games/games/even.py
@@ -4,10 +4,10 @@ from random import randint
 question = 'Answer "yes" if the number is even, otherwise answer "no".'
 
 
-def play_game():  
-        number = randint(1, 500)
-        print(f'Question: {number}')
-        if number % 2 == 0:
-            return 'yes'
-        else:
-            return 'no'
+def play_game():
+    number = randint(1, 500)
+    print(f'Question: {number}')
+    if number % 2 == 0:
+        return 'yes'
+    else:
+        return 'no'
