7cd7c19f9f9cd6e59e0e99a23a79757813b3485c
xantol <xantol@rambler.ru>
2024-06-05 23:33:51 +0500

    update even game module

--- brain_games/games/even.py
+++ brain_games/games/even.py
@@ -1,27 +1,13 @@
-import prompt
 from random import randint
-from brain_games.games.greet import welcome_user
 
 
-def is_even():
-    user = welcome_user()
-    print('Answer "yes" if the number is even, otherwise answer "no".')
-    i = 0
-    while i < 3:
-        random_number = randint(1, 500)
-        if random_number % 2 == 0:
-            correct_answer = 'yes'
+question = 'Answer "yes" if the number is even, otherwise answer "no".'
+
+
+def play_game():  
+        number = randint(1, 500)
+        print(f'Question: {number}')
+        if number % 2 == 0:
+            return 'yes'
         else:
-            correct_answer = 'no'
-        print(f'Question: {random_number}')
-        answer = prompt.string('Your answer: ')
-        if answer == correct_answer:
-            print('Correct!')
-            i += 1
-            if i == 3:
-                print(f'Congratulations, {user}!')
-                break
-        else:
-            print(f"'{answer}' is wrong answer ;(. Correct answer was \
-            '{correct_answer}'.\nLet's try again, {user}!")
-            break
+            return 'no'
--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 
-from brain_games.games.even import is_even
+from brain_games.games import even
+from brain_games.engine import play_engine
 
 
 def main():
-    is_even()
+    play_engine(even)
 
 
 if __name__ == '__main__':
