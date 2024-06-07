d150398e67b5a197b00e5b4050060dd279d3ea70
xantol <xantol@rambler.ru>
2024-06-07 23:03:23 +0500

    update even.py

--- brain_games/games/even.py
+++ brain_games/games/even.py
@@ -4,10 +4,13 @@ from random import randint
 question = 'Answer "yes" if the number is even, otherwise answer "no".'
 
 
+def is_even(number):
+    return number % 2 == 0
+
+
 def play_game():
     number = randint(1, 500)
     print(f'Question: {number}')
-    if number % 2 == 0:
+    if is_even(number):
         return 'yes'
-    else:
-        return 'no'
+    return 'no'
