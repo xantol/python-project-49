d90e1dee20b2f5756aac5399c457a5a1d92c2dce
xantol <xantol@rambler.ru>
2024-06-07 23:03:06 +0500

    update calc.py

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -4,15 +4,19 @@ from random import randint, choice
 question = 'What is the result of the expression'
 
 
-def play_game():
-    num1 = randint(1, 100)
-    num2 = randint(1, 100)
-    operator = choice(['+', '-', '*'])
-    print(f'Question: {num1} {operator} {num2}')
+def calculate(num1, num2, operator):
     if operator == '+':
         correct_answer = num1 + num2
     elif operator == '-':
         correct_answer = num1 - num2
     else:
         correct_answer = num1 * num2
-    return str(correct_answer)
+    return correct_answer
+
+
+def play_game():
+    num1 = randint(1, 100)
+    num2 = randint(1, 100)
+    operator = choice(['+', '-', '*'])
+    print(f'Question: {num1} {operator} {num2}')
+    return str(calculate(num1, num2, operator))
