0b2c85194e938f4c27ec7c4ee3e48edc480bb107
xantol <xantol@rambler.ru>
2024-06-07 23:11:11 +0500

    update gcd.py

--- brain_games/games/gcd.py
+++ brain_games/games/gcd.py
@@ -4,13 +4,17 @@ from random import randint
 question = 'Find the greatest common divisor of given numbers.'
 
 
-def play_game():
-    num1 = randint(1, 100)
-    num2 = randint(1, 100)
-    print(f'Question: {num1} {num2}')
+def calculate_gcd(num1, num2):
     while num1 != 0 and num2 != 0:
         if num1 > num2:
             num1 = num1 % num2
         else:
             num2 = num2 % num1
-    return str(num1 + num2)
+    return num1 + num2
+
+
+def play_game():
+    num1 = randint(1, 100)
+    num2 = randint(1, 100)
+    print(f'Question: {num1} {num2}')
+    return str(calculate_gcd(num1, num2))
