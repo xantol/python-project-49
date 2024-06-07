f83437c6d48960195e51984296a8e81562beb963
xantol <xantol@rambler.ru>
2024-06-07 23:34:55 +0500

    update prime.py

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -4,12 +4,18 @@ from random import randint
 question = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 
 
+def is_prime(num):
+    if num == 2:
+        return True
+    for divider in range(2, num):
+        if num % divider == 0:
+            return False
+    return True
+
+
 def play_game():
     num = randint(2, 100)
     print(f'Question: {num}')
-    if num == 2:
+    if is_prime(num):
         return 'yes'
-    for divider in range(2, num):
-        if num % divider == 0:
-            return 'no'
-    return 'yes'
+    return 'no'
