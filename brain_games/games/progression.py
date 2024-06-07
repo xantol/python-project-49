aaed63b926f10d618868f1926b3d852e94bc1c00
xantol <xantol@rambler.ru>
2024-06-07 23:55:16 +0500

    update progression.py

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -1,15 +1,19 @@
-# нагуглил метод join)
 from random import randint
 
 
 question = 'What number is missing in the progression?'
 
 
-def play_game():
-    progression_list = [randint(0, 50)]
+def make_progression():
+    random_list = [randint(0, 50)]
     step = randint(2, 30)
     for i in range(0, randint(4, 10)):
-        progression_list.append(progression_list[i] + step)
+        random_list.append(random_list[i] + step)
+    return random_list
+
+
+def play_game():
+    progression_list = make_progression()
     index = randint(0, len(progression_list) - 1)
     correct_answer = progression_list[index]
     progression_list[index] = '..'
